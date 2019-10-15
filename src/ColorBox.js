92d41a2d2b53b250da89d84c7a1e4ff649401644
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-10-15 06:43:59 -0700

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import MovieCard from './card-components/MovieCard.js'
+import movieData from './data.js'
 
-export default class ColorBox extends Component {
-  
+export default class MovieShowcase extends Component {
+
+  generateMovieCards = () => {
+    // map over your movieData array and return the correct 
+    return movieData.map(function (title, IMD_Rating, genres, poster) {
+      return MovieCard(title, IMD_Rating, genres, poster);
+    })
+  }
+
   render() {
     return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
+      <div id="movie-showcase">
+        {this.generateMovieCards()}
       </div>
     )
   }
-  
 }
