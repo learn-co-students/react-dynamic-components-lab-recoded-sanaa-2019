81e71dec6ca947f22bc5597c63939d42a85ad727
EmanAlzariey <eman_alzariey@yahoo.com>
2019-11-27 10:13:57 -0800

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,14 @@
 //your code here
+import React, { Component } from 'react';
+
+export default class ColorBox extends Component {
+
+  render() {
+    return (
+        <div className="comment">
+
+         {this.props.commentText}
+
+        </div>
+    )}
+}
